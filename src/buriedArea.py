5dd139c18ef0bfeea223442c94e62349c74be19b
“Rodrigo <rodrigo.faccioli@gmail.com>
2017-02-07 16:10:41 -0200

    Added buried_area_all_res into Cassandra

---
+++ src/buriedArea.py
@@ -0,0 +1,40 @@
+from pyspark.sql import SQLContext, Row
+from commonDatabase import CommonDatabase
+
+from commonFunctions import remover_separator_filename_mode, get_molecule_name_from_filename_mode
+
+
+class BuriedArea(CommonDatabase):
+
+    """
+    	load file that contains the residue list for Buried Area
+    	sc - spark context
+    	path_file_buried_area - path filename of all residues hbond
+    """
+    def load_file_all_residue_buried_area(self, sc, path_file_buried_area):
+    	all_residue	= sc.textFile(path_file_buried_area)
+    	header = all_residue.first() #extract header
+    	#Spliting file by \t
+    	all_residue_split = all_residue.filter(lambda x:x !=header).map(lambda line: line.split("\t"))
+    	all_residue_split = all_residue_split.map(lambda p: Row( residue=str(p[0]), buried_area_residue=float(p[1]), residue_sasa_buried=float(p[2]), pose=str(p[3]) ))
+    	return all_residue_split
+
+    def save_buried_area_all_res_table(self, sqlctx, df):
+    	table = self.client.get_keyspace() + "." + "buried_area_all_res"
+    	buried_area_all_res = sqlctx.sql("select residue, buried_area_residue, residue_sasa_buried, pose from buried_area_all_resFILE")
+    	buried_area_all_resRDD = buried_area_all_res.rdd.map(lambda p: Row( residue=str(p[0]), buried_area_residue=float(p[1]), residue_sasa_buried=float(p[2]), pose=str(p[3]) ))
+    	for row in buried_area_all_resRDD.collect():
+    		sql_query = """
+    			INSERT INTO @$$$$$$$$$$$$@
+    			(pose, buried_area_residue, residue, residue_sasa_buried)
+    			VALUES (?, ?, ?, ?);
+    		"""
+    		sql_query = sql_query.replace("@$$$$$$$$$$$$@", table)
+    		bound_statement = self.client.session.prepare(sql_query)
+    		self.client.session.execute( bound_statement.bind((
+    			row.pose,
+    			row.buried_area_residue,
+    			row.residue,
+    			row.residue_sasa_buried
+    			))
+    		)
--- src/populate_database.py
+++ src/populate_database.py
@@ -5,6 +5,7 @@ from pyspark.sql import SQLContext
 import os, sys
 from loadConfig import load_config
 from hydrogen import Hydrogen
+from buriedArea import BuriedArea
 
 def main():
     #Loading config file from json
@@ -15,7 +16,7 @@ def main():
     separator_receptor = conf_file["virtualScreeningData"]["separatorReceptor"]
     drugdesignCassandra_source_path = conf_file["drugdesignCassandra"]["sourcePath"]
     hydrogen_fileName = os.path.join(conf_file["drugdesignAnalysisFiles"]["rootPathAnalysis"], conf_file["drugdesignAnalysisFiles"]["hydrogenAllRes"])
-    buried_area_fileName = os.path.join(conf_file["drugdesignAnalysisFiles"]["rootPathAnalysis"], conf_file["drugdesignAnalysisFiles"]["hydrogenAllRes"])
+    buried_area_fileName = os.path.join(conf_file["drugdesignAnalysisFiles"]["rootPathAnalysis"], conf_file["drugdesignAnalysisFiles"]["buriedAreasAllRes"])
     keyspace = conf_file["cassandraDB"]["keyspace"]
     nodeIP = conf_file["cassandraDB"]["nodeIP"]
 
@@ -26,6 +27,7 @@ def main():
     sc.addPyFile(os.path.join(drugdesignCassandra_source_path,"connection.py"))
     sc.addPyFile(os.path.join(drugdesignCassandra_source_path,"commonDatabase.py"))
     sc.addPyFile(os.path.join(drugdesignCassandra_source_path,"hydrogen.py"))
+    sc.addPyFile(os.path.join(drugdesignCassandra_source_path,"buriedArea.py"))
 
     # Creating Hydrogen object
     hydrogen = Hydrogen()
@@ -47,9 +49,23 @@ def main():
 
     #Creating Histogram of hydrogen_all_res based on molecule
     hydrogen.save_histogram_hydrogen_all_res_molecule(separator_receptor, separator_filename_mode, sql, df_hydrogen_all_res)
-#############################################################################################
 
     #Closing database connection
     hydrogen.close_connection()
+#############################################################################################
+
+    # Creating Buried Area object
+    buried_area = BuriedArea()
+    buried_area.set_cassandra_client(nodeIP, keyspace)
+
+    #Reading hydrogen all residue file and converting it as dataframe
+    df_buried_area_all_res = sql.createDataFrame( buried_area.load_file_all_residue_buried_area(sc, buried_area_fileName) )
+    df_buried_area_all_res.createOrReplaceTempView("buried_area_all_resFILE")
+
+    #Saving buried_area_all_res Cassandra Table
+    buried_area.save_buried_area_all_res_table(sql, df_buried_area_all_res)
+
+    #Closing database connection
+    buried_area.close_connection()
 
 main()
